2be5950ed0a6584169a7e4be31597d99b3fd56ad
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2025-09-07 14:06:06 +0530

    "changed routes to home"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return <BrowserRouter>
   <Routes>
-    <Route path="/signup" element={<Signup />}/>
+    <Route path="/" element={<Signup />}/>
     <Route path="/signin" element={<Signin />}/>
     <Route path="/dashboard" element={<Dashboard />}/>
   </Routes>
